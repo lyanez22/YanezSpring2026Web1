d37765f65fa25a6e80ae0508baa6d2be97586a3c
Litzy Yanez <143418889+lyanez22@users.noreply.github.com>
2026-05-20 15:50:03 -0400

    Update on comments on how to load website correctly
    Added comments regarding sound playback restrictions and reasoning for music choice.

--- Yanez_Final/music.js
+++ Yanez_Final/music.js
@@ -1,3 +1,8 @@
+//Sound doesn't play when you open a file directly in your browser 
+//(file://) but works on a local server (http://) because of browser security restrictions
+
+//please note sound will play on live server but the website runs smoother on local server 
+
 // music integration: 20191229 by Mac DeMarco
 // reasoning for music choice: I feel like it goes well with the vibe of the game
 // like sense of discovery and little bit of melancholy since the player is kinda alone
